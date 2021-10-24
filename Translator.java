98904ef3d49d738af23af70f27a9f5e3cad892f0
StevenC1337 <stevenc1337@gmail.com>
2021-10-24 16:44:58 -0700

    phase 2 classes (rough outline)

---
+++ Translator.java
@@ -0,0 +1,33 @@
+public class Translator {
+
+    // Identifiers are used for two things: variables and labels.
+    public void addID(String name, String value) {
+        try {
+            if(name == "variable") {
+            /*
+               add variable:
+                   variables are always local to a function;
+                   variable names must be unique within a function.
+                   the first character cannot be a digit or a dot.
+             */
+            }
+            else if(name == "label") {
+            /*
+               add label:
+                   There are three types of labels:
+                        data labels
+                        function labels
+                        code labels
+                   All label names must be unique across the entire program.
+             */
+            }
+            else {
+                throw new Exception("Error: " + name + " is not a variable or label.");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
