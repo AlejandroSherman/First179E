44edca07cd8f0d43c9504945bdc411b1bef2a7e0
StevenC1337 <stevenc1337@gmail.com>
2021-10-24 22:56:47 -0700

    Phase 2 classes (rough outline)

--- Translator.java
+++ Translator.java
@@ -3,6 +3,15 @@ public class Translator { // converts java -> vapor
     // Identifiers
     // Identifiers are used for two things: variables and labels.
     public void addID(String name, String value) {
+        /* Identifiers can be declared with different types in different scopes.
+            Thus, a symbol table that maps identifiers to types is kept during the translation.
+            The type of an identifier is either class, method, field or local variable.
+            The symbol table keeps a stack of scopes.
+            For example:
+                at the beginning of a class, a new scope is pushed and the implicit field "this",
+                the fields and the methods of the class are added to the scope.
+                The main class is handled separately to generate the "Main()" function.
+         */
         try {
             if(name == "variable") {
             /*
@@ -34,6 +43,7 @@ public class Translator { // converts java -> vapor
     // Data Segments
     // Vapor has two types of global data segments:const and var.
     public void addData(String name, String value){
+        // In the translation of each class, its v-table is translated to a const data segment.
         try {
             if(name == "const") {
             /*
@@ -67,6 +77,7 @@ public class Translator { // converts java -> vapor
             - function call: call FunctionAddress (Args...)
             - function return: ret Value
             - call to built-in operation: OpName (Args...)
+        In the translation of each (non-static) method, the implicit this parameter is explicitly added to the parameters of the translated function.
          */
     }
 
@@ -95,6 +106,12 @@ public class Translator { // converts java -> vapor
     // There are two variants of the branch instruction: if and if0
     public void addBranch(){
         /*
+        In the translation of the if statement, we label the beginning of the else statement and the end and use the instruction if0 to goto to the else label if the condition fails.
+        We goto to the end label at the end of the then statement.
+        The while statement can be similarly translated by labeling the start and the end.
+        */
+
+        /*
         add if:
             The if jumps to CodeLabel if Value is non-zero and falls through to the next instruction otherwise.
          */
@@ -117,8 +134,17 @@ public class Translator { // converts java -> vapor
     // Function Call
     public void addFunctionCall(){
         /*
-        add Function Call:
+        In the translation of a method call, if the value of the receiver is zero, a null pointer error is reported.
+        The class type and then the v-table of the receiver object can be retrieved from the symbol table.
+        The offset of the method can be retrieved from the v-table.
+                vt = [rc]      where rc is the receiver
+                f = [vt + d]   where d = o * 4,
+                                     o is the offset of the method retrieved from the v-table
+                r = call f(rc arg1 arg2)
+        */
 
+        /*
+        add Function Call:
             Var = call :FunctionLabel (Args...)
                 The assignment Var = is optional.
                 The arguements list Args... is a whitespace-separated list of value entries: integer literals, variables, or label references.
@@ -144,44 +170,51 @@ public class Translator { // converts java -> vapor
     public void arithmetic(){
         /*
         Basic arithmetic:
-            Add, Sub, Mul, Div, Rem, MulS, DivS, RemS, ShiftL, ShiftR, ShiftLA
+            Add, Sub, MulS
             The "-S" variants operate on signed integers.
-             a = Add(a b)
+            a = Add(a b)
 
         Comparison:
-            Eq, Ne, Lt, Le, LtS, LeS
+            Eq, Lt, LtS
             The "-S" variants operate on signed integers.
-
-        Bitwise boolean operators:
-            And, Or, Not
          */
     }
 
     // Memory Allocation
     public void memAlloc(){
         /*
-        HeapAlloc:
-            take an integer - the number of bytes of memory to allocate.
-            Returns the address of newly-allocated memory.
-
+        In the translation of an object allocation, the class type and then the class record can be retrieved from the symbol table.
+        The allocation size is 4 times the record size plus 4.
+        This is because a word is four bytes and the first word is used to store a reference to the v-table of the object.
+             r  = HeapAllocZ(s)      where s = rs * 4 + 4
+                                               rs = record size
+            [r] = :ClassVTableName
+         */
+        
+        /*
         HeapAllocZ:
+            takes an integer - the number of bytes of memory to allocate.
+            Returns the address of newly-allocated memory.
             The "-Z" variant initializes the memory to all zero.
+            When a field access is translated to a memory operation, the class record is used to determine the offset of the object memory that the memory operation should access.
          */
     }
-    
+
     // Display Output
     public void output(){
-        /* These do not return a value.
-        
-        PrintInt:
-            print out unsigned integers.
-            
+        /*
         PrintIntS:
             print out signed integers.
-            
-        PrintString:
-            print out strings. 
+            Does not return a value.
 
+        Error:
+            is for abnormal program termination (for errors like null pointer deferences, etc).
+            It takes a string message to display to the user.
+
+        DebugPrint:
+            is only for debugging.
+            It accepts any number of values and prints out the interpreter's internal representation of the value.
+            This can be useful for getting information about pointers.
          */
     }
 }
