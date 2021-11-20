52d6f159832267b66ceb20dd24fa6c3bc4cbee70
StevenC1337 <stevenc1337@gmail.com>
2021-11-19 23:51:26 -0800

    updated Interval

--- Interval.java
+++ Interval.java
@@ -1,11 +1,18 @@
-
-
 public class Interval {
 
     public int finishLine;
     public int startLine;
+    public RegLoc location;
 
-    public Interval(int i, int j, RegLoc regLoc) {
+    public Interval(int start, int finish, RegLoc regLoc) {
+        startLine = start;
+        finishLine = finish;
+        location = regLoc;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + startLine + "-" + finishLine + "]";
     }
 
 }
